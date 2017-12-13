813a66c3f6c2c9043960c8b6f5d14d07174aa61f
DenverBYF <851166182@qq.com>
2017-12-13 13:01:51 +0000

    add link

--- app/Http/BC.php
+++ app/Http/BC.php
@@ -13,7 +13,11 @@ class BC
 		}
 		$data = [];
 		$data['text'] = "博客新文章发布";
-		$data['attachments'][] = ['title' => "文章标题 : ".$content['title'], 'text' => "作者 : ".$content['user']];
+		$data['attachments'][] = [
+			'title' => "文章标题 : ".$content['title'],
+			'text' => "作者 : ".$content['user'],
+			'url' => "http://tgblog.helloyzy.cn/",
+		];
 		$client->request('POST', env('BearyChatHook'), [
 			'form_params' => ['payload' => json_encode($data)]
 		]);
